c3885eac1e85d537bbf6f00c209cacea91b4b28c
Amarjeetcoder1 <90110541+Amarjeetcoder1@users.noreply.github.com>
2022-03-10 00:21:14 +0530

    Today

---
+++ Compound_Arithmetic_Operator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int sum=10;
+     int x=5;
+    int prod=10;
+     int y=5;
+
+    sum+=x;
+    prod*=y;
+    cout<<"Sum "<<sum<<endl;
+    cout<<"multi "<<prod<<endl;
+    return 0;
+}
